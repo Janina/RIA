8ba95e503a818b76772d5ae08c42038eaea35caf
Janina <jt22df@student.lnu.se>
2011-02-17 16:14:57 +0100

    test

---
+++ board.js
@@ -0,0 +1,28 @@
+var Board = {
+  
+
+  /* 
+   * Funktionen anropas då sidan öppnas. 
+   * Ritar ut allt som ska vara på sidan vid start. 
+   */
+  init: function() {
+      
+    
+  
+
+  
+  
+  }
+ 
+ 
+   /*
+   * Gör ett formulär för nytt event.
+   */
+  form: function()
+  {
+    
+  }
+ 
+}
+
+window.onload = Board.init;
---
+++ event.js
@@ -0,0 +1,26 @@
+function Event(date, text) //Konstruktorfunktion
+{
+  var eventDiv = document.createElement("div");
+  
+  
+}
+
+Event.insertEvent = function(date, text, user)
+{
+  
+}
+
+Event.deleteEvent = function(date, text, user)
+{
+  
+}
+
+Event.getEventFromDate(date)
+{
+  
+}
+
+Event.getRandomEvent()
+{
+  
+}
