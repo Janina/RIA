4e90cd8868b32241f974ba9b720bff1a2d478555
Janina <jt22df@student.lnu.se>
2011-02-18 09:16:24 +0100

    uppdatering

--- board.js
+++ board.js
@@ -7,10 +7,61 @@ var Board = {
    */
   init: function() {
       
+/*
+    * Anropas när någon klickar på logga in-kanppen. 
+    * Ska ta användaren till facebooks loginsida om man inte redan är inloggad.
+    * Redan inloggad på fb - loggas in här också.  
+    */
+    $("#login").bind("click",function()
+    {
+      
+    });
     
-  
-
-  
+    /*
+     * ska loggas ut från calendarboard genom facebook.
+     */
+    $("#logout").bind("click",function()
+    {
+      
+    });
+    
+    /*
+     * Anropas då användaren trycker på nytt event. 
+     * Formulär visas. 
+     */
+    $("#newEvent").bind("click",function()
+    {
+      
+    });
+    
+    
+    /*
+     * Anropas då anv trycker på spara event.
+     * Värden sätts in i db. Nytt Event-objekt skapas. 
+     */
+    $("#saveEvent").bind("click",function()
+    {
+      
+    });
+    
+    /*
+     * Anropas då man trycker på ett datum i datepickern för visa event. 
+     * Datum och datepicker-instansen som parametrar.
+     * 
+     */
+    $("#datepicker").bind("onSelect",function(date, dp)
+    {
+      
+    });
+    
+    /*
+     * Anropas då man väljer datum i datepickern för spara event. 
+     * sätter date till det valda datumet.
+     */
+    $("#newDate").bind("onSelect",function(date, dp)
+    {
+      
+    });
   
   }
  
